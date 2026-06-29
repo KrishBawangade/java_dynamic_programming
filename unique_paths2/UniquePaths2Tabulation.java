67d8442aef6189f3e32407b948147bbf0802e262
KrishBawangade <krishbawangade08@gmail.com>
2026-06-29 08:13:20 +0530

    Unique Paths2 Dynamic Programming Tabulation

---
+++ unique_paths2/UniquePaths2Tabulation.java
@@ -0,0 +1,46 @@
+package unique_paths2;
+
+public class UniquePaths2Tabulation {
+    public static void main(String[] args){
+
+        int[][] obstacleGrid = {{0,0,0},{0,1,0},{0,0,0}};
+
+        int m = obstacleGrid.length;
+        int n= obstacleGrid[0].length;
+
+        int[][] dp = new int[m][n];
+
+        
+
+        for(int i=0; i<m; i++){
+            for(int j = 0; j<n; j++){
+
+                if(obstacleGrid[i][j] == 1){
+                    dp[i][j] = 0;
+                    continue;
+                }
+
+                if(i == 0 && j==0){
+                    dp[i][j] = 1;
+                    continue;
+                }
+
+                int up = 0;
+                int left = 0;
+
+
+                if(i>0){
+                    up = dp[i-1][j]; 
+                }
+
+                if(j>0){
+                    left = dp[i][j-1];
+                }
+
+                dp[i][j] = up+left;
+            }
+        }
+
+        System.out.println(dp[m-1][n-1]);
+    }
+}
