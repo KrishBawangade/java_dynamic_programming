33df33df5ffb7637f977337085df235ca67101af
KrishBawangade <krishbawangade08@gmail.com>
2026-06-30 09:01:17 +0530

    Minimum Path Sum Dynamic Programming Tabulation

---
+++ minimum_path_sum/MinimumPathSumTabulation.java
@@ -0,0 +1,40 @@
+package minimum_path_sum;
+
+public class MinimumPathSumTabulation {
+    public static void main(String[] args){
+
+        int[][] grid = {{1,3,1},{1,5,1},{4,2,1}};
+
+        int m = grid.length;
+        int n= grid[0].length;
+
+        int[][] dp = new int[m][n];
+
+        dp[0][0] = grid[0][0];
+
+        for(int i=0; i<m; i++){
+            for(int j = 0; j<n; j++){
+
+                if(i == 0 && j==0){
+                    continue;
+                }
+
+                int up = Integer.MAX_VALUE;
+                int left = Integer.MAX_VALUE;
+
+
+                if(i>0){
+                    up = grid[i][j] + dp[i-1][j]; 
+                }
+
+                if(j>0){
+                    left = grid[i][j] + dp[i][j-1];
+                }
+
+                dp[i][j] = Math.min(up, left);
+            }
+        }
+
+        System.out.println(dp[m-1][n-1]);
+    }
+}
