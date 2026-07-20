0112a3a1a5a72a72903eca32ef5a7483093fad09
KrishBawangade <krishbawangade08@gmail.com>
2026-07-20 07:12:14 +0530

    Longest Palindromic Subsequence Dynamic Programming

---
+++ longest_palindrome_subseq/LPS.java
@@ -0,0 +1,43 @@
+package longest_palindrome_subseq;
+
+import java.util.Arrays;
+
+public class LPS {
+
+    public static int longestCommonSubsequence(String text1, String text2){
+        int l1 = text1.length();
+        int l2 = text2.length();
+
+        int[] prev = new int[l2+1];
+        int[] curr = new int[l2+1];
+
+        for(int i=1; i<=l1; i++){
+            for(int j=1; j<=l2; j++){
+
+                if(text1.charAt(i-1) == text2.charAt(j-1)){
+                    curr[j] = 1+ prev[j-1];
+                }else{
+                    
+                    curr[j] = Math.max(
+                        prev[j],
+                        curr[j-1]
+                    );
+                }
+            }
+            int[] temp = prev;
+            prev = curr;
+            curr = temp;
+
+            Arrays.fill(curr, 0);
+        }
+
+        return prev[l2];
+    }
+
+    public static void main(String[] args){
+        String text1 = "abcdba";
+        String text2 = new StringBuilder(text1).reverse().toString();
+
+        System.out.println(longestCommonSubsequence(text1, text2));
+    }
+}
