99f02b063720300521b9ba46e44610e006189693
KrishBawangade <krishbawangade08@gmail.com>
2026-07-22 05:22:36 +0530

    Min Insertions/Deletions required to convert String1 to String2 Dynamic Programming

---
+++ min_operations_convert_string/MinOperationsConvertString.java
@@ -0,0 +1,48 @@
+package min_operations_convert_string;
+
+import java.util.Arrays;
+
+public class MinOperationsConvertString {
+
+    public static int longestCommonSubsequence(String text1, String text2){
+        int l1 = text1.length();
+        int l2 = text2.length();
+
+        int[] prev = new int[l2+1];
+        int[] curr = new int[l2+1];
+
+        for(int i=1; i<=l1; i++){
+            for(int j=1; j<=l2; j++){
+
+                if(text1.charAt(i-1) == text2.charAt(j-1)){
+                    curr[j] = 1+ prev[j-1];
+                }else{
+                    
+                    curr[j] = Math.max(
+                        prev[j],
+                        curr[j-1]
+                    );
+                }
+            }
+            int[] temp = prev;
+            prev = curr;
+            curr = temp;
+
+            Arrays.fill(curr, 0);
+        }
+
+        return prev[l2];
+    }
+
+    public static void main(String[] args){
+        String text1 = "abcd";
+        String text2 = "anc";
+
+        int commonLen = longestCommonSubsequence(text1, text2);
+
+        int insertions = text2.length() - commonLen;
+        int deletions = text1.length() - commonLen;
+
+        System.out.println(insertions+deletions);
+    } 
+}
